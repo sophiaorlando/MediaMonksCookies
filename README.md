20bff84db1e410fb16bb228007eddd15579f3229
Sophia Orlando <sophiaorlando@sophias-mbp.lan>
2022-11-09 00:52:51 -0500

    finalized project

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
-# MediaMonksCookies
+
+<a name="readme-top"></a>
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/github_username/repo_name">
+    <img src="assets/mmbanner.png" alt="Logo" width="100%" height="80">
+  </a>
+
+<h3 align="center">Media Monks Cookie Challenge</h3>
+
+  <p align="center">
+    Built by Sophia Orlando
+</div>
+
+
+
+<details>
+  <ol>
+    <li>
+      <a>About The Project</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#roadmap">Roadmap</a></li>
+    <li><a href="#contributing">Contributing</a></li>
+    <li><a href="#license">License</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
+
+
+
+<!-- ABOUT THE PROJECT -->
+## About The Project
+
+This project stores and saves cookies in your browser. Every time a user visits the page, or refreshes it, they will be presented with a ball. A cookie will be stored every time the user sees a blue ball or a red ball. Once the cookies are cached, the application will refresh and all previous cookies will be deleted. 
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+### Built With
+
+* HTML
+* CSS
+* Javscript
+* Google Fonts
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+To set up this application, clone this repository on your machine, or download this folder. Open this project inside of your IDE and open your Live Server. 
+
+Once opened and running on your Live Server, right-click to "Inspect" the page and navigate to "Application", then, "Cookies". Click on the box starting with, "http". There, you will see the created, stored, and updated cookies, everytime you visit or refresh the page. 
+
+Visual Studio
+* https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer 
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+<!-- ROADMAP -->
+## Issues
+
+- [ ] Can not show same color ball user saw previously
+- [ ] CSS Styling will be improved
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+<!-- CONTACT -->
+## Contact
+
+
+Project Link: [https://github.com/github_username/repo_name](https://github.com/github_username/repo_name)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
--- index.js
+++ index.js
@@ -32,23 +32,33 @@ function GetCookie (name) {
     var cval = GetCookie (name);
     document.cookie = name + "=" + cval + "; expires=" + exp.toGMTString();
     }
+
     var expDays = 30;
     var exp = new Date();
     exp.setTime(exp.getTime() + (expDays*24*60*60*1000));
-    
+    var sub = document.getElementById("amount")
+    var newSub = document.getElementById("newSub")
+
+
     function amt(){
-    var count = GetCookie('count')
+    var count = GetCookie('test cookie')
     if(count == null) {
-    SetCookie('count','1')
+    SetCookie('test cookie','1')
+    var text = document.createTextNode("You have visited 1 number of times")
+    sub.appendChild(text)
     return 1
     }
     else {
     var newcount = parseInt(count) + 1;
-    DeleteCookie('count')
-    SetCookie('count',newcount,exp)
+    DeleteCookie('test cookie')
+    SetCookie('test cookie',newcount,exp)
+    var text = document.createTextNode("You have visited " + newcount + " number of times")
+    sub.appendChild(text)
     return newcount
        }
+       
     }
+
     function getCookieVal(offset) {
     var endstr = document.cookie.indexOf (";", offset);
     if (endstr == -1)
@@ -57,17 +67,116 @@ function GetCookie (name) {
     }
     amt();
 
+
+function randomImg(){
+    console.log("Media Monks Cookie Challenge by Sophia Orlando!")
+    const ballLinks = ["./assets/blueBall.png", "./assets/redBall.png"]
+    const randomBall = ballLinks.sort(function(){return Math.random() - 0.5})
+    const ballEl = randomBall.pop()
+    choosenImage = ballEl;
+    balls.src = choosenImage;
+    balls.id = choosenImage;
+
+    console.log(choosenImage)
+        return choosenImage
+}
+
+function redBallImg(ball){
+    rImg = ball
+    balls.src = rImg
+}
+
+function blueBallImg(ball){
+    bbImg = ball
+    balls.src = bbImg
+    }
+
+
+var redSub = document.getElementById("redAmount")
+var blueSub = document.getElementById("blueAmount")
+var otherBlueSub = document.getElementById("otherBlueAmount")
+var otherRedSub = document.getElementById("otherRedAmount")
+var getImage = document.getElementById("balls")
+
+
+
+
     function ballShown(){
-        let images = ["./assets/blueBall.png", "./assets/redBall.png"];
-        let imagesLength = images.length;
-        let randomNumber = Math.random();
-        randomNumber = randomNumber*imagesLength;
-        randomNumber = Math.floor(randomNumber);
-        choosenImage = images[randomNumber];
-        balls.src = choosenImage;
-        document.cookie="ball="+choosenImage
-        console.log(choosenImage)
-        return choosenImage;
+
+        const ballCtn = GetCookie("ballCtn")
+        const redBall = GetCookie("redBall")
+        const blueBall = GetCookie("blueBall")
+        // const ballLinks = ["./assets/blueBall.png", "./assets/redBall.png"]
+        // const randomBall = ballLinks.sort(function(){return Math.random() - 0.5})
+        // const ballEl = randomBall.pop()
+        const holder = randomImg()
+
+         if(ballCtn == null && holder == "./assets/redBall.png"){
+            SetCookie('ballCtn','1')
+            SetCookie('redBall', '1')
+            SetCookie('blueBall', "0")
+            var redText = document.createTextNode("You have seen the red ball 1 number of times")
+            redSub.appendChild(redText)
+            var altBlueText = document.createTextNode("You have seen the blue ball 0 number of times")
+            otherBlueSub.appendChild(altBlueText)
+
+            return 1
+
+        } 
+        else if (ballCtn == null && holder == "./assets/blueBall.png" ){
+            SetCookie('ballCtn','1')
+            SetCookie('blueBall', '1')
+            SetCookie('redBall', '0')
+            var blueText = document.createTextNode("You have seen the blue ball 1 number of times")
+            blueSub.appendChild(blueText)
+            var altRedText = document.createTextNode("You have seen the red ball 0 number of times")
+            otherRedSub.appendChild(altRedText)
+
+            return 1
+
+        } if (ballCtn != null && holder == "./assets/redBall.png") {
+            const newBallCtn = parseInt(ballCtn) + 1;
+            const newRedCtn = parseInt(redBall) + 1;
+            const newBlueCtn2 = parseInt(blueBall) + 0;
+
+            DeleteCookie('ballCtn')
+            DeleteCookie('redBall')
+            DeleteCookie('blueBall')
+            SetCookie("ballCtn", newBallCtn, exp)
+            SetCookie("redBall", newRedCtn, exp)
+            SetCookie("blueBall", newBlueCtn2, exp)
+            var redText2 = document.createTextNode("You have seen the red ball "+ newRedCtn+" number of times")
+            redSub.appendChild(redText2)
+            var blueAltText = document.createTextNode("You have seen the blue ball "+ newBlueCtn2+" number of times")
+            otherBlueSub.appendChild(blueAltText)
+            redBallImg("./assets/redBall.png");
+
+            return newRedCtn
+
+        } if (ballCtn != null && holder == "./assets/blueBall.png" ){
+            const newBallCtn = parseInt(ballCtn) + 1;
+            const newBlueCtn = parseInt(blueBall) + 1;
+            const newRedCtn2 = parseInt(redBall) + 0;
+            DeleteCookie('ballCtn')
+            DeleteCookie('blueBall')
+            DeleteCookie('redBall')
+            SetCookie("ballCtn", newBallCtn, exp)
+            SetCookie("blueBall", newBlueCtn, exp)
+            SetCookie("redBall", newRedCtn2, exp)
+            var blueText2 = document.createTextNode("You have seen the blue ball "+ newBlueCtn+" number of times")
+            blueSub.appendChild(blueText2)
+            var altRedText2 = document.createTextNode("You have seen the red ball "+ newRedCtn2 + " number of times")
+            otherRedSub.appendChild(altRedText2)
+            blueBallImg("./assets/blueBall.png");
+         
+            return newBlueCtn
+        } 
+
+        return ;
     }
     ballShown();
+
+
+
+
  
